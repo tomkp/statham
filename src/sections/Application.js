92582a5bb4747826d241f0e16939ae33edeb4b7f
tomkp <tom@tomkp.com>
2016-04-15 16:59:27 +0100

    ...

--- src/sections/Application.js
+++ src/sections/Application.js
@@ -17,7 +17,27 @@ The Ex-Presidents rip off banks to finance their endless summer!`
     {id: 1, name: 'Warchild', value: `You stand straight up, you'll just fall over, so let's try it again.`},
     {id: 2, name: 'Bunker', value: `You're dragging your foot, you're gonna be fish food.`},
     {id: 3, name: 'Bodhi', value: `Caught my first tube this morning,... sir!`},
-    {id: 4, name: 'Panda', value: `I'm a panda`}
+    {id: 4, name: 'Panda', value: `Johnny paddles rapidly, ducks under.
+Sees another, bigger wave coming.
+Pissed off... at himself, at the downy-cheeked hotshots,
+at the frustration, he turns his board around and starts
+paddling hard.
+He somehow gets the soles of his feet in contact with the
+top of the board, then struggles up.  He's standing --
+sort of.
+Arms pinwheeling, he topples in a nasty crash...
+Right in front of a SHAVED-HEAD SURFER on full
+afterburner.
+
+Johnny vanishes in an explosion of spray.  His board
+SHOOTS OUT.
+It SMASHES SIDEWAYS INTO RAZORHEAD.
+The guy does an ugly endo.
+Utah comes up GASPING for air, arms flailing.
+His board, floating a few feet away, tugging at his ankle.
+He drapes his torso across the board and pants for breath.
+Razorhead, already back on his board, paddles over.
+Points to a small dent in the fiberglass.`}
 ];
 
 
