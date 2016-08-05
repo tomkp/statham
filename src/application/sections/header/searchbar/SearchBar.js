28731bbedee83e4188b22bc4b438cea203e3e6a3
tomkp <tom@tomkp.com>
2016-08-05 15:32:34 +0100

    search bar

--- src/application/sections/header/searchbar/SearchBar.js
+++ src/application/sections/header/searchbar/SearchBar.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import './searchbar.scss';
 
-export default () => {
-    return <div className="searchbar">
-        <input />
-    </div>
-};
+
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchTerm: '',
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        this.setState({
+            searchTerm: event.target.value,
+        });
+    }
+
+    render() {
+        return (
+            <div className="searchbar">
+                <input onChange={this.handleInputChange} />
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
