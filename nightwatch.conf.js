2c3e70eeb2917f069d9a293098f9b365c5f53e1b
tomkp <tom@tomkp.com>
2016-08-15 10:49:34 +0100

    ...

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,4 +1,41 @@
-{
+const SELENIUM_CONFIGURATION = {
+  start_process: true,
+  server_path: 'bin/selenium-server-standalone-2.53.1.jar',
+  host: '127.0.0.1',
+  port: 4444,
+  cli_args : {
+    "webdriver.chrome.driver" : "bin/chromedriver"
+  }
+};
+
+const CHROME_CONFIGURATION = {
+  browserName: 'chrome',
+  javascriptEnabled: true,
+  acceptSslCerts: true,
+  chromeOptions : {
+    args : ["start-fullscreen"]
+  }
+};
+
+const DEFAULT_CONFIGURATION = {
+  launch_url: 'http://localhost',
+  selenium_port: 4444,
+  selenium_host: 'localhost',
+  desiredCapabilities: CHROME_CONFIGURATION
+};
+
+const ENVIRONMENTS = {
+  default: DEFAULT_CONFIGURATION
+};
+
+module.exports = {
+  src_folders: ['test/ui'],
+  output_folder : "log",
+  selenium: SELENIUM_CONFIGURATION,
+  test_settings: ENVIRONMENTS
+};
+
+/*{
   "src_folders" : ["test"],
   "output_folder" : "log",
 
@@ -19,7 +56,7 @@
       "selenium_port"  : 4444,
       "selenium_host"  : "localhost",
       "silent": true,
-      "filter" : "test/ui/*-test.js",
+      "filter" : "test/ui/!*-test.js",
       "screenshots" : {
         "enabled" : true,
         "on_failure" : true,
@@ -35,4 +72,4 @@
       }
     }
   }
-}
+}*/
